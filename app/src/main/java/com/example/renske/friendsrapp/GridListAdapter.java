92d93afb5c837a2bc580aef99a9248c967c85eba
Renske <vluuks@gmail.com>
2018-02-11 18:41:36 +0100

    adapter n stuff

--- app/src/main/java/com/example/renske/friendsrapp/GridListAdapter.java
+++ app/src/main/java/com/example/renske/friendsrapp/GridListAdapter.java
@@ -3,9 +3,12 @@ package com.example.renske.friendsrapp;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,16 +19,32 @@ import java.util.List;
 
 public class GridListAdapter extends ArrayAdapter {
 
+    ArrayList<User> userList;
+
+
+
+
     public GridListAdapter(@NonNull Context context, int resource, @NonNull ArrayList<User> objects) {
         super(context, resource, objects);
+
+        userList = objects;
     }
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
 
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
 
-        
-        return super.getView(position, convertView, parent);
+        ImageView image = convertView.findViewById(R.id.profile_image);
+        TextView text = convertView.findViewById(R.id.user_name);
+
+        User currentUser = userList.get(position);
+        image.setImageResource(currentUser.getDrawableId());
+        text.setText("De TimTest!!");
+
+        return convertView;
     }
 }
--- app/src/main/java/com/example/renske/friendsrapp/MainActivity.java
+++ app/src/main/java/com/example/renske/friendsrapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.renske.friendsrapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
 
 import java.util.ArrayList;
@@ -19,20 +20,29 @@ public class MainActivity extends AppCompatActivity {
 
         userList = new ArrayList<>();
 
-        User testTim = new User("Tim", "hoi ik ben tim ik hou van chips", 23, R.drawable.tim);
+        User testTim = new User("Tim",
+                                "hoi ik ben tim ik hou van chips",
+                                23,
+                                getResources().getIdentifier("tim", "drawable", getPackageName())
+        );
+
+        // add 5 tims for the test
         userList.add(testTim);
         userList.add(testTim);
         userList.add(testTim);
         userList.add(testTim);
         userList.add(testTim);
+
+
+        // set adapter and shit
+        setGridView();
 
     }
 
     public void setGridView(){
 
         GridView gridView = findViewById(R.id.grid_view);
-
-
-
+        ArrayAdapter<User> adapter = new GridListAdapter(this, R.layout.list_item, userList);
+        gridView.setAdapter(adapter);
     }
 }
