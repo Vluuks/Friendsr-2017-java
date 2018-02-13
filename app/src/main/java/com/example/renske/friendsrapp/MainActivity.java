950e54f1f981de9ad8619985b9303f7bfb17f404
Renske <vluuks@gmail.com>
2018-02-13 15:46:32 +0100

    arrays aslist used to add everyone + onclick

--- app/src/main/java/com/example/renske/friendsrapp/MainActivity.java
+++ app/src/main/java/com/example/renske/friendsrapp/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.renske.friendsrapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,53 +22,86 @@ public class MainActivity extends AppCompatActivity {
 
         // Create users to populate our app
 
-        userList = new ArrayList<>();
+        User test = new User("bla", "bla", 1, getResources().getIdentifier("tim", "drawable", getPackageName()));
 
-        User testTim = new User("Tim",
-                                "hoi ik ben tim ik hou van chips",
-                                23,
-                                getResources().getIdentifier("tim", "drawable", getPackageName())
-        );
-        User testMartijn = new User("Martijn",
-                                "Ik ben Martijn, a1.20 is echt altijd koud!",
-                                35,
-                                    getResources().getIdentifier("martijn", "drawable", getPackageName())
-        );
-        User testJulia = new User("Julia",
-                "oeh moeilijke keus",
-                22,
-                getResources().getIdentifier("julia", "drawable", getPackageName())
-        );
-        User testMarijn = new User("Marijn",
-                "IK BEN EEN HACKER",
-                22,
-                getResources().getIdentifier("marijn", "drawable", getPackageName())
-        );
+        //userList = new ArrayList<>();
+        userList = new ArrayList<User>(Arrays.asList(
 
+                new User("Tim",
+                        "hoi ik ben tim ik hou van chips",
+                        23,
+                        getResources().getIdentifier("tim", "drawable", getPackageName())),
 
-        // add 5 tims for the test
-        userList.add(testTim);
-        userList.add(testMartijn);
-        userList.add(testJulia);
-        userList.add(testMarijn);
-        userList.add(testTim);
-        userList.add(testMartijn);
-        userList.add(testJulia);
-        userList.add(testMarijn);
-        userList.add(testTim);
-        userList.add(testMartijn);
-        userList.add(testJulia);
-        userList.add(testMarijn);
+                new User("Renske",
+                        "cactussen zijn cool",
+                        24,
+                        getResources().getIdentifier("renske", "drawable", getPackageName())),
+
+                new User("Martijn",
+                        "Ik ben Martijn, a1.20 is echt altijd koud!",
+                        35,
+                        getResources().getIdentifier("martijn", "drawable", getPackageName())),
+
+                new User("Julia",
+                        "oeh moeilijke keus",
+                        22,
+                        getResources().getIdentifier("julia", "drawable", getPackageName())),
+
+                new User("Marijn",
+                        "IK BEN EEN HACKER",
+                        25,
+                        getResources().getIdentifier("marijn", "drawable", getPackageName())),
+
+                new User("Emma",
+                        "WOEHOE",
+                        22,
+                        getResources().getIdentifier("emma", "drawable", getPackageName())),
+
+                new User("Mirja",
+                        "WOEHOE",
+                        22,
+                        getResources().getIdentifier("mirja", "drawable", getPackageName())),
+
+                new User("Quinten",
+                        "WOEHOE",
+                        22,
+                        getResources().getIdentifier("quinten", "drawable", getPackageName())),
+
+                new User("Sascha",
+                        "WOEHOE",
+                        22,
+                        getResources().getIdentifier("sascha", "drawable", getPackageName())),
+
+                new User("Simon",
+                        "WOEHOE",
+                        22,
+                        getResources().getIdentifier("simon", "drawable", getPackageName())),
+
+                new User("Wouter",
+                        "ja",
+                        22,
+                        getResources().getIdentifier("wouter", "drawable", getPackageName()))
+
+        ));
 
         // set adapter and shit
         setGridView();
 
     }
 
-    public void setGridView(){
-
+    public void setGridView() {
         GridView gridView = findViewById(R.id.grid_view);
         ArrayAdapter<User> adapter = new GridListAdapter(this, R.layout.list_item, userList);
         gridView.setAdapter(adapter);
+
+        AdapterView.OnItemClickListener listener = new OnItemClickListenerGrid();
+        gridView.setOnItemClickListener(listener);
+    }
+
+    private class OnItemClickListenerGrid implements AdapterView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            Log.d("YO", "klinkerdieklink");
+        }
     }
 }
